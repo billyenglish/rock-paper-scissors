d186a30ecf805acde0158fe751110924c0285d5e
Demar English <demarenglish@Demars-MacBook-Pro.local>
2024-03-16 21:11:03 -0400

    Revised my project refactoring

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('#result');
+/* Game Logic for Rock, Paper, Scissors. */
 
-function getComputerChoice() {  
-    const randomChoice = ['rock', 'paper', 'scissors'];
-    let randomNumber = Math.floor(Math.random() * randomChoice.length);
-    return randomChoice[randomNumber];
+let userChoice = prompt("Please select Rock, Paper, or Scissors");
+
+function getComputerChoice() {
+    let computerChoice = ["Rock", "Paper", "Scissors"];
+    let randomChoice = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[randomChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -12,24 +13,22 @@ function playRound(playerSelection, computerSelection) {
     let computer = computerSelection.toLowerCase();
     
     if (player === computer) {
-        display.textContent = `You Tie! ${player} equals ${computer}`;
-    } else if (player === 'rock' && computer === 'scissors') {
-        display.textContent = `You Win! ${player} beats ${computer}`;
-    } else if (player === 'scissors' && computer === 'paper') {
-        display.textContent = `You Win! ${player} beats ${computer}`;
-    } else if (player === 'paper' && computer === 'rock') {
-        display.textContent = `You Win! ${player} beats ${computer}`;
+        console.log(`YOU TIED! ${player} equals ${computer}`);
+    } else if (player === "rock" && computer === "scissors") {
+        console.log(`YOU WON! ${player} beats ${computer}`);
+    } else if (player === "paper" && computer === "rock") {
+        console.log(`YOU WON! ${player} beats ${computer}`);
+    } else if (player === "scissors" && computer === "paper") {
+        console.log(`YOU WON! ${player} beats ${computer}`);
     } else {
-        display.textContent = `You Lose! ${computer} beats ${player}`;
+        console.log(`YOU LOSE! ${computer} beats ${player}`)
     }
 }
 
+function playGame() {
+    let result = getComputerChoice();
+    console.log(playRound(userChoice, result)); 
+}
 
+playGame();
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let computerChoice = getComputerChoice();
-        playRound(button.id, computerChoice);
-    });
-});
-    
