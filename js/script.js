a127bae1a2eae7108e9577d93f0529b237566ea3
Demar English <demarenglish@Demars-MacBook-Pro.local>
2024-05-24 06:48:13 -0400

    Adding updates to the interactivity

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-const gameResult = document.querySelector('#game-result');
-const playerScoreDisplay = document.querySelector('#player-score');
-const computerScoreDisplay = document.querySelector('#computer-score');
-const gameButtons = document.querySelectorAll('button');
-const resetDisplay = document.querySelector(".reset-game");
+const gameDisplay = document.querySelector("#game-display");
+const playerScoreDisplay = document.querySelector("#player-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+const gameButtons = document.querySelectorAll("#game-controls button");
+const resetDisplay = document.querySelector(".game-result");
 const resetButton = document.querySelector("#reset");
+const gameWinner = document.querySelector("#game-winner");
 let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    const randomChoice = ["rock", "paper", "scissors"];
-    return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+    const choice = ["rock", "paper", "scissors"];
+    const random = Math.floor(Math.random() * choice.length);
+    return choice[random];
 }
 
-function determineWinner(playerSelection, computerSelection) {
+function winScenario(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         return "It's a tie!";
@@ -23,81 +25,69 @@ function determineWinner(playerSelection, computerSelection) {
     ) {
         return "Player Wins!";
     } else {
-        return "Computer Wins!"
+        return "Computer Wins!";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
 
-    const winner = determineWinner(playerSelection, computerSelection);
+    const result = winScenario(playerSelection, computerSelection);
 
-    if (winner === "It's a tie!") {
-        gameResult.innerHTML = `<p>It's a tie! <span class="selection">${playerSelection}</span> ties <span class="selection">${computerSelection}</span></span></p>`
-        gameResult.style.color = "white";
-    }
-
-    if (winner === "Player Wins!") {
-        gameResult.innerHTML = `<p>Player Wins! <span class="selection">${playerSelection}
-        </span> beats <span class="selection">${computerSelection}</span></p>`
-        gameResult.style.color = "white";
-    }
-
-    if (winner === "Computer Wins!") {
-        gameResult.innerHTML = `<p>Computer Wins! <span class="selection">${computerSelection}
-        </span> beats <span class="selection">${playerSelection}</span></p>`
-        gameResult.style.color = "white";
+    if (result === "It's a tie!") {
+        gameDisplay.innerHTML = `<p>It's a tie! Both selected ${playerSelection}</p>`;
+    } else if (result === "Player Wins!") {
+        gameDisplay.innerHTML = `<p>Player Wins! <span>${playerSelection}</span> beats <span>${computerSelection}</span></p>`;
+    } else {
+        gameDisplay.innerHTML = `<p>Computer Wins! <span>${computerSelection}</span> beats <span>${playerSelection}</span></p>`;
     }
 }
 
 function playGame(playerSelection, computerSelection) {
 
-    const gameResults = determineWinner(playerSelection, computerSelection);
+    const gameResult = winScenario(playerSelection, computerSelection);
 
-    if (gameResults === "Player Wins!") {
+    if (gameResult === "Player Wins!") {
         playerScore++;
-        playerScoreDisplay.innerHTML = playerScore;
-        playerScoreDisplay.style.color = "white";
-    }
-
-    if (gameResults === "Computer Wins!") {
+        playerScoreDisplay.textContent = playerScore;
+    } else if (gameResult === "Computer Wins!") {
         computerScore++;
-        computerScoreDisplay.innerHTML = computerScore;
-        computerScoreDisplay.style.color = "white";
+        computerScoreDisplay.textContent = computerScore;
     }
 
     if (playerScore === 5 || computerScore === 5) {
-        gameButtons.forEach((button) => {
-            if (button !== reset) {
-                button.disabled = true;
-            }
-        });
-
-        if (computerScore === 5) {
-            gameResult.innerHTML = `<p>Game Over. Computer Wins ${playerScore} - ${computerScore}</p>`;
-        } else {
-            gameResult.innerHTML = `<p>Game Over. Computer Wins ${playerScore} - ${computerScore}</p>`;
+        if (playerScore === 5) {
+            gameDisplay.innerHTML = `<p>Player Wins the Game</p>`;
+            gameWinner.innerHTML = "Player Wins!";
+            resetGame();
+        } else if (computerScore === 5) {
+            gameDisplay.innerHTML = `<p>Computer Wins the Game</p>`;
+            gameWinner.innerHTML = "Computer Wins!";
+            resetGame();
         }
-        resetDisplay.style.display = "block";
+        gameButtons.forEach((button) => {
+            button.disabled = true;
+        });
+        resetDisplay.style.visibility = "visible";
     }
 }
 
 function resetGame() {
     playerScore = 0;
     computerScore = 0;
-    playerScoreDisplay.innerHTML = 0;
-    computerScoreDisplay.innerHTML = 0;
-    gameResult.innerHTML = "";
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    gameDisplay.innerHTML = "Let's Play!";
+    resetDisplay.style.visibility = "hidden";
     gameButtons.forEach((button) => {
         button.disabled = false;
     });
-    resetDisplay.style.display = "none";
 }
 
 resetButton.addEventListener("click", resetGame);
 
 gameButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        const playerSelection = button.id;
+        const playerSelection = button.dataset.choice;
         const computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
         playGame(playerSelection, computerSelection);
